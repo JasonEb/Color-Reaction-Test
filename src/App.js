2cc102475e1c284d8ac488071daaf46124ef317c
holyoke <jasonebueng@gmail.com>
2019-08-21 20:43:09 -0700

    sets up color shuffler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState("blue");
+  const [choice, setChoice] = useState("");
+
+  const handleKeyDown = (e) => {
+    if (e.key === " "){
+      shuffleColors()
+    } 
+  }
+
+  const shuffleColors = () => {
+    let colors = ["red", "blue", "yellow", "green"]
+    let rngIdx = Math.floor(Math.random() * colors.length)
+    setColor(colors[rngIdx])
+  }
+
+  const answerButtons = () => {
+    let colors = ["red", "blue", "yellow", "green"]
+    return colors.map((color, idx) => <div key={color}>{idx + 1}. {color}</div>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={handleKeyDown} tabIndex="1">
+      <div>Color: {color}</div>
+      <br />
+      <div>CHOICES
+        {answerButtons()}
+      </div>
     </div>
   );
 }
